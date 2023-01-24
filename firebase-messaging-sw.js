2dcce8b3cdf9514852046126eee0172ce94e9276
sureshsu <suresh.pgsem2008@gmail.com>
2023-01-24 19:01:32 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -35,7 +35,7 @@
  // messages.
  const messaging = firebase.messaging();
  if ('serviceWorker' in navigator) {
-   navigator.serviceWorker.register('../firebase-messaging-sw.js')
+   navigator.serviceWorker.register('/firebase-messaging-sw.js')
     .then(function(registration) {
       console.log('Registration successful, scope is:', registration.scope);
     }).catch(function(err) {
